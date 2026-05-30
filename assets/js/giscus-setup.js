5baa1e1c810ce83a8c1f0593808f430d613206ef
TJ-WANG-OK <wtj_3@qq.com>
2026-05-30 13:11:16 +0000

    Deploying to gh-pages from @ TJ-WANG-OK/TJ-WANG-OK.github.io@47778aa7b098195ebb0cf6be9c5965b1630cf52f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "TJ-WANG-OK/Personal-CV",
+    "data-repo": "TJ-WANG-OK/TJ-WANG-OK.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
